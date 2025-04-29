0706dfc417793e60a9c07aabe9af6eff7e18e761
swaswa999 <swayams@icloud.com>
2025-04-28 19:33:33 -0700

    see no reason this will not work so no need to change :D

--- KrAkenSwerve/src/main/java/frc/robot/subsystems/swerve/SingleModuleSwerveSubsystem.java
+++ KrAkenSwerve/src/main/java/frc/robot/subsystems/swerve/SingleModuleSwerveSubsystem.java
@@ -1,5 +1,27 @@
 package frc.robot.subsystems.swerve;
 
-public class SingleModuleSwerveSubsystem {
-    
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SingleModuleSwerveSubsystem extends SubsystemBase{    
+
+    SwerveModule module;
+    double MAX_VEL = 1; //change this
+
+    public SingleModuleSwerveSubsystem(SwerveModule module) {
+        this.module = module;
+    }
+
+    public void setDrivePowers(double xPower, double yPower) {
+        double velocity = MAX_VEL * Math.sqrt(yPower * yPower + xPower * xPower) / Math.sqrt(2);
+        double angle = Math.atan2(yPower, xPower);
+
+        module.setDesiredState(new SwerveModuleState(velocity, new Rotation2d(angle)));
+    }
+
+    public void setState(double speed, double angleRads) {
+        module.setDesiredState(new SwerveModuleState(speed, new Rotation2d(angleRads)));
+    }
+
 }
