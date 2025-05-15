d4a85aff38cbeeff8856fcb9f7f6d73a8d649c03
swaswa999 <swayams@icloud.com>
2025-05-15 13:40:50 -0700

    robot container ready to run  :D now rlly all that needs to be done is test

--- KrAkenSwerve/src/main/java/frc/robot/RobotContainer.java
+++ KrAkenSwerve/src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,21 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.swerve.SwerveModule;
+// frc imports
+import frc.robot.controllers.PS5DriveController;
 
+// Subsystems
+import frc.robot.subsystems.swerve.SwerveSubsystem;
+
+// WPILib imports
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.subsystems.swerve.KrakenSwerveModule;
-import frc.robot.subsystems.swerve.KrakenSwerveModule;
-import frc.robot.subsystems.swerve.SingleModuleSwerveSubsystem;
+
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,38 +27,84 @@ import frc.robot.subsystems.swerve.SingleModuleSwerveSubsystem;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  CommandXboxController ps5Controller = new CommandXboxController(0); 
 
-  private final KrakenSwerveModule swerveModule = new KrakenSwerveModule(1, 2, 0.0, 3);// dont change to SwerveModule or it will use CTRE swerve
-  private SingleModuleSwerveSubsystem singleModuleSwerveSubsystem = new SingleModuleSwerveSubsystem(swerveModule);
+  private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+  private boolean isCompetition = true;
+
+  private PS5DriveController driveController;
+  private CommandPS5Controller mechController;
+  private SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-
-    ps5Controller.leftBumper().onTrue(new InstantCommand(() -> {
-      singleModuleSwerveSubsystem.setState(0.1, Math.PI);
-    }));
-
-    ps5Controller.rightBumper().onTrue(new InstantCommand(() -> {
-      singleModuleSwerveSubsystem.setState(-0.1, 0);
-    }));
-
-  
-
-    // Configure the trigger bindings
+    constructDriveController(); 
+    constructMechController();
     configureBindings();
   }
 
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the
    * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
+   * predicate, or via the named f`actories in {@link
    * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
    * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
    * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
+      /* Driving -- One joystick controls translation, the other rotation. If the robot-relative button is held down,
+      * the robot is controlled along its own axes, otherwise controls apply to the field axes by default. If the
+      * swerve aim button is held down, the robot will rotate automatically to always face a target, and only
+      * translation will be manually controllable. */
+    swerveSubsystem.setDefaultCommand(
+      new RunCommand(() -> {
+        swerveSubsystem.setDrivePowers(
+          driveController.getForwardPower(),
+          driveController.getLeftPower(),
+          driveController.getRotatePower()
+        );
+        }, 
+        swerveSubsystem
+      )
+    );
+      
+    driveController.getRelativeMode().whileTrue(
+      new RunCommand(
+        () -> {
+          swerveSubsystem.setRobotRelativeDrivePowers(
+            driveController.getForwardPower(),
+            driveController.getLeftPower(),
+            driveController.getRotatePower()
+          );
+          }, swerveSubsystem)
+    );
+
+
+    /* Pressing the button resets the field axes to the current robot axes. */
+    driveController.bindDriverHeadingReset(
+      () ->{
+        swerveSubsystem.resetDriverHeading();
+      },
+      swerveSubsystem
+    );
   }
+
+  /**
+   * Constructs the drive controller based on the name of the controller at port
+   * 0
+   */
+  private void constructDriveController(){
+    driveController = new PS5DriveController();
+    driveController.setDeadZone(0.05);
+  }
+
+  /**
+   * Constructs mech controller
+   */
+  private void constructMechController(){
+    mechController = new CommandPS5Controller(1);
+  }
+
+
 }
